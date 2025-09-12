f6efeed3b528a0bcd26218f6244d307c23631ece
Hieu1607 <nguyenhieu16072004@gmail.com>
2025-09-12 09:05:51 +0700

    fix: update CORS policy

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 # Load environment variables and configure Gemini API
@@ -22,6 +23,20 @@ setup_logging()
 logger = get_logger(__name__)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "https://ai-legal-assistant-ui.onrender.com",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(retrieve.router)
 app.include_router(rag.router)
