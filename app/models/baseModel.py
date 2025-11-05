c00780bce55c5650c86d67ddad844ccae14a0b19
Hieu1607 <hieu@example.com>
2025-11-05 16:31:42 +0700

    fix: update limit characters to entry question

--- app/models/baseModel.py
+++ app/models/baseModel.py
@@ -11,5 +11,8 @@ class RAGRequest(BaseModel):
     """
 
     query: str = Field(
+        description="The question to ask.",
+        min_length=5,
+        max_length=200,
         json_schema_extra={"example": "Chương I điều 1 luật tố tụng dân sự mới nhất ?"}
     )
